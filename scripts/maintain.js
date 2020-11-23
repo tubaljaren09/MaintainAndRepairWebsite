ec88fc38cb631fc6ad2d9ae3acccbc3b4fc7c978
Jaren <58203530+tubaljaren09@users.noreply.github.com>
2020-11-23 15:37:08 +0800

    reset

--- scripts/maintain.js
+++
@@ -1,5 +0,0 @@
-function scheduleClick(){
-    document.getElementById('schedule').addEventListener('click', function() {
-      document.querySelector('.bg-modal').style.display='flex';
-    });
-  }
