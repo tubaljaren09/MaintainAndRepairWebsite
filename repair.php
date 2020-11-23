4fed3e5e50706e537429703410af9109ab793c33
Jaren <58203530+tubaljaren09@users.noreply.github.com>
2020-11-23 15:36:08 +0800

    reset

--- repair.php
+++ repair.php
@@ -26,50 +26,7 @@
     </div>
     <div class="bg-modal">
         <div class="modal-content">
-            <div class="left">
-                <h3>Select a product</h3>
-                <div class="products">
-                    <div class="reficon">
-                        <img id="reficon" src="./images/reficon.png" onclick="refClick()"> 
-                        <p>Refrigerator</p>
-                    </div>
-                    <div class="airconicon">
-                        <img id="airconicon" src="./images/airconicon.png" onclick="airconClick()">
-                        <p>Aircon</p>
-                    </div>
-                    <div class="washingicon">
-                        <img id="washingicon" src="./images/washingicon.png" onclick="washingClick()">
-                        <p>Washing Machine</p>
-                    </div>
-                </div>
-                <div class="selectbrands">
-                    <h3>Select the brand</h3>
-                    <div class="conduraicon">
-                        <div class="conduraicon">
-                        <img src="./images/conduralogo.png">
-                    </div>
-                    <div class="lgicon">
-                        <img src="./images/lglogo.png">
-                    </div>
-                    <div class="panasonicicon">
-                        <img src="./images/panasoniclogo.png">
-                    </div>
-                    <div class="toshibaicon">
-                        <img src="./images/toshibalogo.png">
-                    </div>
-                </div>
-            </div>
-            <div class="right">
-            <form class="info" action="/maintainandrepair/repair.php" method="post">
-                <label>Product:</label>
-                <label class="productlabel" id="productlabel"></label>
-                <input class="product" type="hidden" name="product" id="product">
-                <label>Brand:</label>
-                <label class="productbrand" id="productbrand"></label>
-                <input class="brand" type="hidden" name="brand" id="brand">
-            </form>
-            <button id="close" onclick="closeClick()">CLOSE</button>
-            </div>
+           
         </div>
     </div>
 </body>
